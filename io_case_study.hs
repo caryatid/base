017b05b9049a15baf09497de84ed1c9a21f2638a
dave <david.setchell@gmail.com>
2013-01-22 16:56:00 -0800

    foomore

--- io_case_study.hs
+++ io_case_study.hs
@@ -2,8 +2,49 @@
 --
 
 import RecursiveContents
+import System.Directory (Permissions(..), getModificationTime, getPermissions)
+import Control.Monad (filterM)
+import System.Time (ClockTime(..))
+import System.FilePath (takeExtension)
+import Control.Exception (bracket, handle)
+import System.IO (IOMode(..), hClose, hFileSize, openFile)
+import Data.Time
+
+type Predicate = FilePath
+    -> Permissions
+    -> Maybe Integer
+    -> UTCTime
+    -> Bool
+
+getFileSize :: FilePath -> IO (Maybe Integer)
+getFileSize a = undefined
+
+betterFind :: Predicate -> FilePath -> IO [FilePath]
+
+betterFind p path = getRecursiveContents path >>= filterM check
+    where check name = do
+            perms <- getPermissions name
+            size <- getFileSize name
+            modified <- getModificationTime name
+            return (p name perms size modified)
+
+simpleFileSize path = do
+    h <- openFile path ReadMode
+    size <- hFileSize h
+    hClose h
+    return size
+
+
+saferFileSize path = handle (\_ -> return Nothing) $ do
+    h <- openFile path ReadMode
+    size <- hFileSize h
+    hClose h
+    return (Just size)
 
 simpleFind :: (FilePath -> Bool) -> FilePath -> IO [FilePath]
 simpleFind p path = do 
     names <- getRecursiveContents path
     return (filter p names)
+
+
+    
